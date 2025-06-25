e5a1f97ec449f7d754963fce0bce3381737c80c6
kchurch-maker <church.karen@gmail.com>
2025-06-25 22:44:06 +0100

    Update about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,10 +16,10 @@ const About = () => {
             Before Intercom, I spent 8 years in industrial research labs — Yahoo Labs and Telefonica Research — leading teams of scientists and conducting research of my own focused on understanding, mining and modeling mobile user behaviours with a view to a shaping new product innovation. I hold a PhD in Computer Science and have published 50 peer reviewed papers at top-tier conferences in areas like human computer interaction, mobile computing, information retrieval and recommender systems.
           </p>
           <p>
-            I'm founder of HER+Data, a community to support and connect women who work with & love data. I enjoy writing and speaking about my experiences in research, data science and leadership. I love meeting and connecting with others so feel free to drop me an email or say hi on Twitter or LinkedIn.
+            I founded HER+Data, a community to support and connect women who work with & love data. I enjoy writing and speaking about my experiences in research, data science and leadership. I love meeting and connecting with others so feel free to drop me an email or say hi on Twitter or LinkedIn.
           </p>
           <p>
-            I live in Dublin, Ireland with my husband, two children and chocolate labrador. I'm a big foodie, amateur baker and curious crafter. I like lifting heavy weights and and good glass of red wine!
+            I live in Dublin, Ireland with my husband, two children and chocolate labrador. I'm a big foodie, amateur baker and curious crafter. I like lifting heavy weights and a good glass of red wine!
           </p>
         </div>
       </div>
